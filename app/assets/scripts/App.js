d50c2a8b72cefa3df392f230e367cf706c3a2758
Pabliz <36200735+pabliz@users.noreply.github.com>
2021-11-03 21:06:55 +0100

    first commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
 import "../styles/styles.css";
 
-import Canvas from './modules/Canvas';
-import SwapSpan from "./modules/SwapSpan";
-
+// import Canvas from './modules/Canvas';
+// import SwapSpan from "./modules/SwapSpan";
+// import Scroll from "./modules/scroll";
 import gsap from "gsap";
 
 
@@ -10,40 +10,44 @@ import gsap from "gsap";
 if (module.hot) {
     module.hot.accept();
 }
-new Canvas({
-    dom: document.getElementById('container')
-});
+
+// new Scroll();
+// new Canvas({
+//     dom: document.getElementById('container')
+// });
 // PRELOADER
 
-const preloader = document.querySelector(".preloader");
-const main = document.querySelector("main");
+// const preloader = document.querySelector(".preloader");
+// const main = document.querySelector("main");
 
-window.addEventListener("load", () => {
 
-    gsap.to(preloader, {
-        delay: 2,
-        duration: .1,
-        autoAlpha: 0
-    })
-    gsap.from(main, {
-        background: "#17191C",
-        delay: 1,
-        duration: .3,
-    })
-})
+// window.addEventListener("load", () => {
+
+
+//     gsap.to(preloader, {
+//         delay: 2,
+//         duration: .1,
+//         autoAlpha: 0
+//     })
+//     gsap.from(main, {
+//         background: "#17191C",
+//         delay: 1,
+//         duration: .3,
+//     })
+// })
 
 // CURSOR
-const cursor = document.querySelector(".cursor")
+// const cursor = document.querySelector(".cursor")
 
-document.addEventListener('mousemove', (e) => {
-    gsap.to(cursor, {
-        x: (-e.pageX),
-        height: e.pageY,
-    })
-})
+// document.addEventListener('mousemove', (e) => {
+//     gsap.to(cursor, {
+//         x: (-e.pageX),
+//         height: e.pageY,
+//     })
+// })
 
-document.addEventListener("dblclick", () => {
-        cursor.classList.toggle("hidden");
-    })
-    // SWAPSPAN
-new SwapSpan();
+// document.addEventListener("dblclick", () => {
+//         cursor.classList.toggle("hidden");
+//     })
+// SWAPSPAN
+// new SwapSpan();
