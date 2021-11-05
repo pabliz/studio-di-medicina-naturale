800bedcf6d93bba77c93969a1557eb43f4e76e7e
Pabliz <36200735+pabliz@users.noreply.github.com>
2021-11-05 18:12:50 +0100

    gsap animations

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,8 +4,8 @@ import "../styles/styles.css";
 // import SwapSpan from "./modules/SwapSpan";
 // import Scroll from "./modules/scroll";
 import gsap from "gsap";
-
-
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger)
 
 if (module.hot) {
     module.hot.accept();
@@ -51,3 +51,131 @@ if (module.hot) {
 //     })
 // SWAPSPAN
 // new SwapSpan();
+
+
+// TITLE SPANS ////////////////////////////////////////////////
+let t1 = document.querySelector(".t1")
+let t2 = document.querySelector(".t2")
+let t3 = document.querySelector(".t3")
+
+    let tl = gsap.timeline()
+    tl.from(t1, {
+        duration: 1,
+        rotate: 30,
+        scaleX: 2,
+        scaleY: 0,
+        transformOrigin: 'bottom left',
+        autoAlpha: 0,
+        ease: 'Power2.in'})
+        .from(t2, {
+            duration: .9,
+            rotate: -30,
+            scaleX: 2,
+            scaleY: 0,
+            transformOrigin: 'bottom right',
+            autoAlpha: 0,
+            ease: 'Power2.in',
+    }, '-=0.7')
+    .from(t3, {
+        duration: .8,
+        rotate: -30,
+        scaleX: 2,
+        scaleY: 0,
+        transformOrigin: 'bottom right',
+        autoAlpha: 0,
+        ease: 'Power2.in',
+}, '-=0.7')
+// Hero + Navigation //////////////////////////////////////////////////////////
+let hero = document.querySelector(".hero")
+let nav = document.querySelector("nav")
+
+gsap.timeline().from(hero, {
+    duration: 1.5,
+    // rotate: 90,
+    scale: 2,
+    // scaleY: 0,
+    transformOrigin: 'center center',
+    autoAlpha: 0,
+    ease: 'Power1.in'})
+    .from(nav, {
+        duration: 1,
+    y: -200,
+    autoAlpha: 0,
+    }, '-=.7')
+// Hero Image ////////////////////////////////////////////////////
+let heroImg = document.querySelector(".hero__img")
+gsap.to(heroImg, {
+    duration: 1,
+    rotate: 90,
+    scaleX: 2,
+    scaleY: 0,
+    transformOrigin: 'bottom right',
+    autoAlpha: 0,
+    ease: 'Power1.in',
+    scrollTrigger: {
+        //markers: true,
+        trigger: heroImg,
+        start: 'top 35%',
+        end: 'bottom 10%',
+        toggleActions:"restart none reverse reset" // onEnter onLeave onEnterBack onLeaveBack
+            // options: play pause resume reset restart complete reverse none
+    }
+})
+
+// Section titles ///////////////////////////////////////////////////////
+let sectionTitles = document.querySelectorAll("h2.section__info__title")
+sectionTitles.forEach(title=>{  
+    gsap.from(title, {
+        duration: 1,
+        xPercent: -100,
+        autoAlpha: 0,
+        ease: "power1.out",
+        scrollTrigger: {
+            trigger: title,
+            //markers: true,
+            start: 'top 100%',
+            end: "bottom 60%",
+            scrub: 3
+            //toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+            // options: play pause resume reset restart complete reverse none
+        }
+    })
+})
+// section images //////////////////////////////////////////////////////////
+let images = document.querySelectorAll("img.img")
+images.forEach(img=>{  
+    gsap.from(img, {
+        duration: 1.5,
+        y: 350,
+        scale: 1.1,
+        autoAlpha: 0,
+        ease: "power1.out",
+        scrollTrigger: {
+            trigger: img,
+            //markers: true,
+            start: 'top 110%',
+            end: "bottom 25%",
+            toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+            // options: play pause resume reset restart complete reverse none
+        }
+    })
+})
+// Paragraphs ////////////////////////////////////////////////////////
+let sectionTexts = document.querySelectorAll(".section__info")
+sectionTexts.forEach(text=>{  
+    gsap.from(text, {
+        duration: 1,
+        y: 350,
+        scale: 1.1,
+        autoAlpha: 0,
+        ease: "power1.out",
+        scrollTrigger: {
+            trigger: text,
+            //markers: true,
+            start: 'top 110%',
+            end: "bottom 25%",
+            toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+            // options: play pause resume reset restart complete reverse none
+        }
+    })
+})
