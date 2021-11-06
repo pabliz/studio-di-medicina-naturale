6092b72577ce774ce9193f42c4570f3b8ede509b
Pabliz <36200735+pabliz@users.noreply.github.com>
2021-11-06 09:17:15 +0100

    prelader + tweak to intro animation timeline

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -17,23 +17,12 @@ if (module.hot) {
 // });
 // PRELOADER
 
-// const preloader = document.querySelector(".preloader");
-// const main = document.querySelector("main");
+const preloader = document.querySelector(".preloader");
+const main = document.querySelector("main");
 
 
 // window.addEventListener("load", () => {
 
-
-//     gsap.to(preloader, {
-//         delay: 2,
-//         duration: .1,
-//         autoAlpha: 0
-//     })
-//     gsap.from(main, {
-//         background: "#17191C",
-//         delay: 1,
-//         duration: .3,
-//     })
 // })
 
 // CURSOR
@@ -53,13 +42,36 @@ if (module.hot) {
 // new SwapSpan();
 
 
-// TITLE SPANS ////////////////////////////////////////////////
 let t1 = document.querySelector(".t1")
 let t2 = document.querySelector(".t2")
 let t3 = document.querySelector(".t3")
+let hero = document.querySelector(".hero")
+let nav = document.querySelector("nav")
+let heroImg = document.querySelector(".hero__img")
+let sectionTitles = document.querySelectorAll("h2.section__info__title")
+let images = document.querySelectorAll("img.img")
+let sectionTexts = document.querySelectorAll(".section__info")
 
+const hidePreloader = () => {
+    gsap
+}
+
+const init = async () => {
+    // HOME INTRO ANIMATION ///////////////////////////////////////
+    // PRELOADER OFF //////////////////////////////////////////////
     let tl = gsap.timeline()
-    tl.from(t1, {
+    tl.to(preloader, {
+        delay: 2,
+        duration: .1,
+        autoAlpha: 0
+    })
+    .from(main, {
+        background: "#17191C",
+        delay: 1,
+        duration: .3,
+    })
+    // TITLE SPANS ////////////////////////////////////////////////
+        .from(t1, {
         duration: 1,
         rotate: 30,
         scaleX: 2,
@@ -75,107 +87,108 @@ let t3 = document.querySelector(".t3")
             transformOrigin: 'bottom right',
             autoAlpha: 0,
             ease: 'Power2.in',
-    }, '-=0.7')
-    .from(t3, {
-        duration: .8,
-        rotate: -30,
-        scaleX: 2,
-        scaleY: 0,
-        transformOrigin: 'bottom right',
-        autoAlpha: 0,
-        ease: 'Power2.in',
-}, '-=0.7')
-// Hero + Navigation //////////////////////////////////////////////////////////
-let hero = document.querySelector(".hero")
-let nav = document.querySelector("nav")
+        }, '-=0.7')
+        .from(t3, {
+            duration: .8,
+            rotate: -30,
+            scaleX: 2,
+            scaleY: 0,
+            transformOrigin: 'bottom right',
+            autoAlpha: 0,
+            ease: 'Power2.in',
+        }, '-=0.7')
+        // Hero + Navigation //////////////////////////////////////////////////////////
+        .from(hero, {
+            duration: 1.5,
+            // rotate: 90,
+            scale: 2,
+            // scaleY: 0,
+            transformOrigin: 'center center',
+            autoAlpha: 0,
+            ease: 'Power1.in'})
+            .from(nav, {
+                duration: .5,
+                y: -20,
+                autoAlpha: 0,
+            }, '-=.7')
+        // END INTRO //////////////////////////////////////////////////////////////////
 
-gsap.timeline().from(hero, {
-    duration: 1.5,
-    // rotate: 90,
-    scale: 2,
-    // scaleY: 0,
-    transformOrigin: 'center center',
-    autoAlpha: 0,
-    ease: 'Power1.in'})
-    .from(nav, {
-        duration: 1,
-    y: -200,
-    autoAlpha: 0,
-    }, '-=.7')
-// Hero Image ////////////////////////////////////////////////////
-let heroImg = document.querySelector(".hero__img")
-gsap.to(heroImg, {
-    duration: 1,
-    rotate: 90,
-    scaleX: 2,
-    scaleY: 0,
-    transformOrigin: 'bottom right',
-    autoAlpha: 0,
-    ease: 'Power1.in',
-    scrollTrigger: {
-        //markers: true,
-        trigger: heroImg,
-        start: 'top 35%',
-        end: 'bottom 10%',
-        toggleActions:"restart none reverse reset" // onEnter onLeave onEnterBack onLeaveBack
-            // options: play pause resume reset restart complete reverse none
-    }
-})
+            // Hero Image ////////////////////////////////////////////////////
+            gsap.to(heroImg, {
+                duration: 1,
+                rotate: 90,
+                scaleX: 2,
+                scaleY: 0,
+                transformOrigin: 'bottom right',
+                autoAlpha: 0,
+                ease: 'Power1.in',
+                scrollTrigger: {
+                    //markers: true,
+                    trigger: heroImg,
+                    start: 'top 35%',
+                    end: 'bottom 10%',
+                    toggleActions:"restart none reverse reset" // onEnter onLeave onEnterBack onLeaveBack
+                    // options: play pause resume reset restart complete reverse none
+                }
+            })
+            
+            // Section titles ///////////////////////////////////////////////////////
+            
+            sectionTitles.forEach(title=>{  
+                gsap.from(title, {
+                    duration: 1,
+                    xPercent: -100,
+                    autoAlpha: 0,
+                    ease: "power1.out",
+                    scrollTrigger: {
+                        trigger: title,
+                        //markers: true,
+                        start: 'top 100%',
+                        end: "bottom 60%",
+                        scrub: 3
+                        //toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+                        // options: play pause resume reset restart complete reverse none
+                    }
+                })
+            })
+            // section images //////////////////////////////////////////////////////////
+            
+            images.forEach(img=>{  
+                gsap.from(img, {
+                    duration: 1.5,
+                    y: 350,
+                    scale: 1.1,
+                    autoAlpha: 0,
+                    ease: "power1.out",
+                    scrollTrigger: {
+                        trigger: img,
+                        //markers: true,
+                        start: 'top 110%',
+                        end: "bottom 25%",
+                        toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+                        // options: play pause resume reset restart complete reverse none
+                    }
+                })
+            })
+            // Paragraphs ////////////////////////////////////////////////////////
+            
+            sectionTexts.forEach(text=>{  
+                gsap.from(text, {
+                    duration: 1,
+                    y: 350,
+                    scale: 1.1,
+                    autoAlpha: 0,
+                    ease: "power1.out",
+                    scrollTrigger: {
+                        trigger: text,
+                        //markers: true,
+                        start: 'top 110%',
+                        end: "bottom 25%",
+                        toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
+                        // options: play pause resume reset restart complete reverse none
+                    }
+                })
+            })
+}
 
-// Section titles ///////////////////////////////////////////////////////
-let sectionTitles = document.querySelectorAll("h2.section__info__title")
-sectionTitles.forEach(title=>{  
-    gsap.from(title, {
-        duration: 1,
-        xPercent: -100,
-        autoAlpha: 0,
-        ease: "power1.out",
-        scrollTrigger: {
-            trigger: title,
-            //markers: true,
-            start: 'top 100%',
-            end: "bottom 60%",
-            scrub: 3
-            //toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
-            // options: play pause resume reset restart complete reverse none
-        }
-    })
-})
-// section images //////////////////////////////////////////////////////////
-let images = document.querySelectorAll("img.img")
-images.forEach(img=>{  
-    gsap.from(img, {
-        duration: 1.5,
-        y: 350,
-        scale: 1.1,
-        autoAlpha: 0,
-        ease: "power1.out",
-        scrollTrigger: {
-            trigger: img,
-            //markers: true,
-            start: 'top 110%',
-            end: "bottom 25%",
-            toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
-            // options: play pause resume reset restart complete reverse none
-        }
-    })
-})
-// Paragraphs ////////////////////////////////////////////////////////
-let sectionTexts = document.querySelectorAll(".section__info")
-sectionTexts.forEach(text=>{  
-    gsap.from(text, {
-        duration: 1,
-        y: 350,
-        scale: 1.1,
-        autoAlpha: 0,
-        ease: "power1.out",
-        scrollTrigger: {
-            trigger: text,
-            //markers: true,
-            start: 'top 110%',
-            end: "bottom 25%",
-            toggleActions:"restart pause resume reset" // onEnter onLeave onEnterBack onLeaveBack
-            // options: play pause resume reset restart complete reverse none
-        }
-    })
-})
+window.addEventListener('load', init)
